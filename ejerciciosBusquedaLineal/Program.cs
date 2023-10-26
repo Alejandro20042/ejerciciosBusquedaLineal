81659481b02aed5aa60d1c4544deef06687e1134
rigolook <rixxormen@gmail.com>
2023-10-26 08:23:07 -0700

    Agregar archivos de proyecto.

---
+++ ejerciciosBusquedaLineal/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+namespace ejerciciosBusquedaLineal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 1, 3, 4, 6, 8, 10 };
+            string[] cadenas = { "uno", "dos", "tres", "cuatro", "cinco" };
+
+            // Búsqueda lineal para números enteros
+            Console.Write("Ingrese un número para buscar en la lista de enteros: ");
+            int numeroBuscado = Convert.ToInt32(Console.ReadLine());
+            int resultadoEnteros = Ejercicios.BusquedaLinealEnteros(numeros, numeroBuscado);
+            if (resultadoEnteros != -1)
+            {
+                Console.WriteLine("El número se encuentra en la posición: " + resultadoEnteros);
+            }
+            else
+            {
+                Console.WriteLine("El número no se encuentra en la lista de enteros.");
+            }
+
+            // Búsqueda lineal para cadenas
+            Console.Write("Ingrese una cadena para buscar en la lista de cadenas: ");
+            string cadenaBuscada = Console.ReadLine();
+            int resultadoCadenas = Ejercicios.BusquedaLinealCadenas(cadenas, cadenaBuscada);
+            if (resultadoCadenas != -1)
+            {
+                Console.WriteLine("La cadena se encuentra en la posición: " + resultadoCadenas);
+            }
+            else
+            {
+                Console.WriteLine("La cadena no se encuentra en la lista de cadenas.");
+            }
+
+            // Búsqueda lineal para encontrar el primer número par
+            int resultadoNumeroPar = Ejercicios.BusquedaLinealNumeroPar(numeros);
+            if (resultadoNumeroPar != -1)
+            {
+                Console.WriteLine("El primer número par se encuentra en la posición: " + resultadoNumeroPar);
+            }
+            else
+            {
+                Console.WriteLine("No se encontraron números pares en la lista de enteros.");
+            }
+
+            // Búsqueda lineal para encontrar todos los números pares
+            Console.WriteLine("Posiciones de los números pares en la lista de enteros:");
+            Ejercicios.BuscarNumerosPares(numeros);
+        }
+    }
+}
---
+++ ejerciciosBusquedaLineal/ejercicio.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerciciosBusquedaLineal
+{
+    public class Ejercicios
+    {
+        public static int BusquedaLinealEnteros(int[] numeros, int numeroBuscado)
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] == numeroBuscado)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static int BusquedaLinealCadenas(string[] cadenas, string cadenaBuscada)
+        {
+            for (int i = 0; i < cadenas.Length; i++)
+            {
+                if (cadenas[i] == cadenaBuscada)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static int BusquedaLinealNumeroPar(int[] numeros)
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] % 2 == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static void BuscarNumerosPares(int[] numeros)
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] % 2 == 0)
+                {
+                    Console.WriteLine("Número par encontrado en la posición: " + i);
+                }
+            }
+        }
+    }
+}
